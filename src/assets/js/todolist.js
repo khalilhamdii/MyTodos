f510ef5cca609187e5e884e23ec52e1d54caf720
khalilhamdii <khaalil.hamdi@gmail.com>
2020-12-16 13:25:18 +0100

    Add task input method

--- src/assets/js/todolist.js
+++ src/assets/js/todolist.js
@@ -1,30 +1,96 @@
-const TodoList = (()=>{
-    const renderProjectInput = () => {
-        const inputDiv = document.getElementById("input-div")
-        inputDiv.innerHTML += `
-        <div data-bs-hover-animate="pulse" style="font-size: 20px;color: rgb(255,255,255);margin-left: -15px;padding-left: 20px;">
-        <div><input type="text" id="input" style="width: 120px;border-style: none;border-radius: 15px;margin-right: 10px;font-size: 12px;height: 35px;padding: 0 6px;" placeholder="Enter project name..."><i class="fa fa-check" id="check-icon" style="margin-right: 7px;color: rgb(255,193,7);"></i><i class="fa fa-remove" id="remove-icon" style="color: rgb(255,193,7);"></i></div>
-        </div>
-        `
-    }
+const TodoList = (() => {
+  const renderProjectInput = () => {
+    const inputDiv = document.getElementById("project-input");
+    inputDiv.innerHTML = `
+          <div data-bs-hover-animate="pulse" style="font-size: 20px;color: rgb(255,255,255);margin-left: -15px;padding-left: 20px;">
+          <div><input type="text" id="input" style="width: 120px;border-style: none;border-radius: 15px;margin-right: 10px;font-size: 12px;height: 35px;padding: 0 6px;" placeholder="Enter project name..."><i class="fa fa-check" id="check-icon" style="margin-right: 7px;color: rgb(255,193,7);"></i><i class="fa fa-remove" id="remove-icon" style="color: rgb(255,193,7);"></i></div>
+          </div>
+          `;
+  };
 
-    const removeProjectInput = () => {
-        const inputDiv = document.getElementById("input-div")
-        const div = inputDiv.children[Array.from(inputDiv.children).length-1]
-        inputDiv.removeChild(div)
-    }
+  const renderTaskInput = () => {
+    const taskInput = document.getElementById("new-task-input");
+    taskInput.innerHTML = `
+      <div class="row no-gutters d-flex flex-row"
+      style="margin: 5px;margin-top: 15px;color: rgb(255,255,255);background: rgba(23,162,184,0.5);border: 0.5px solid rgb(255,193,7) ;border-bottom-style: solid;border-bottom-color: rgb(255,193,7);">
+      <div class="col-4 col-sm-5 col-md-6 col-lg-5 d-flex justify-content-start align-items-center"><input
+          type="text" class="w-100"
+          style="border-style: none;padding-left: 20px;border-radius: 15px;line-height: 30px;background: rgb(255,193,7);height: 32px;"
+          placeholder="Enter task name..." required="" minlength="3" maxlength="15"></div>
+      <div
+        class="col-2 col-sm-2 col-md-2 col-lg-2 d-flex d-md-flex d-lg-flex justify-content-sm-center justify-content-md-center justify-content-lg-center"
+        style="padding-right: 5px;padding-left: 5px;"><select class="w-100"
+          style="background: transparent;color: rgb(255,193,7);border-style: none;">
+          <option value="undefined">Priority</option>
+          <option value="4">Very High</option>
+          <option value="3">High</option>
+          <option value="2">Medium</option>
+          <option value="1">Low</option>
+        </select></div>
+      <div class="col-4 col-sm-3 col-md-3 col-lg-4 d-md-flex d-lg-flex align-items-lg-center"
+        style="padding-right: 10px;padding-left: 10px;border-style: none;"><input class="d-lg-flex w-100"
+          type="date"
+          style="border-style: none;padding-right: 20px;padding-left: 20px;border-radius: 15px;line-height: 30px;color: rgb(0,0,0);background: rgb(255,193,7);height: 32px;">
+      </div>
+      <div
+        class="col-2 col-md-1 col-lg-1 d-flex d-md-flex d-lg-flex justify-content-center align-items-center justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center">
+        <i class="fa fa-check" style="color: rgb(255,193,7);"></i><i class="fa fa-remove"
+          style="color: rgb(255,193,7);margin-left: 10px;"></i>
+      </div>
+    </div>
+          `;
+  };
 
-    const addProject = () => {
-        const ul = document.getElementById("add-project")
-        const input = document.getElementById("input").value
-        ul.innerHTML+=`
-        <li data-bs-hover-animate="pulse"
-              style="font-size: 20px;color: rgb(255,255,255);margin-left: -15px;padding-left: 20px;"><strong>${input}</strong></li>
-        `
-    }
+  const renderTask = () => {
+    const tasks = document.getElementById("tasks");
+    tasks.innerHTML += `
+      <div class="row no-gutters d-flex flex-row" data-bs-hover-animate="pulse"
+  style="margin: 5px;margin-top: 15px;color: rgb(255,255,255);border-width: 0.5px;border-style: none;border-bottom-style: solid;border-bottom-color: rgb(255,193,7);">
+  <div class="col-4 col-sm-5 col-md-6 col-lg-5 d-flex justify-content-start align-items-center">
+    <div class="form-check"><input class="form-check-input d-lg-flex align-items-lg-center" type="checkbox"
+        id="formCheck-6" style="border-radius: 0px;"><label
+        class="form-check-label d-lg-flex align-items-lg-center" for="formCheck-1"><strong>Task
+          name</strong></label></div>
+  </div>
+  <div
+    class="col-2 col-sm-2 col-md-2 col-lg-2 d-flex d-md-flex d-lg-flex justify-content-sm-center justify-content-md-center justify-content-lg-center">
+    <span class="d-sm-flex">Priority</span>
+  </div>
+  <div
+    class="col-4 col-sm-3 col-md-3 col-lg-4 d-md-flex d-lg-flex justify-content-md-center justify-content-lg-center">
+    <span class="d-flex justify-content-center align-items-center">Task date&nbsp;</span>
+  </div>
+  <div
+    class="col-2 col-md-1 col-lg-1 d-flex d-md-flex d-lg-flex justify-content-center align-items-center justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center">
+    <i class="fa fa-edit" style="color: rgb(255,193,7);"></i><i class="fa fa-trash"
+      style="color: rgb(255,193,7);"></i>
+  </div>
+  </div>
+          `;
+  };
 
-    return {renderProjectInput, removeProjectInput, addProject}
-}
-)();
+  const removeProjectInput = () => {
+    const inputDiv = document.getElementById("project-input");
+    const div = inputDiv.children[Array.from(inputDiv.children).length - 1];
+    inputDiv.removeChild(div);
+  };
 
-export default TodoList
+  const addProject = () => {
+    const ul = document.getElementById("add-project");
+    const input = document.getElementById("input").value;
+    ul.innerHTML += `
+          <li data-bs-hover-animate="pulse"
+                style="font-size: 20px;color: rgb(255,255,255);margin-left: -15px;padding-left: 20px;"><strong>${input}</strong></li>
+          `;
+  };
+
+  return {
+    renderProjectInput,
+    removeProjectInput,
+    addProject,
+    renderTaskInput,
+    renderTask,
+  };
+})();
+
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+// import start
+
 import "./assets/css/styles.min.css";
-import TodoList from './assets/js/todolist'
+import TodoList from "./assets/js/todolist";
 
 const importAll = (r) => r.keys().map(r);
 importAll(require.context("./assets/js/", true, /\.js$/));
@@ -8,19 +10,37 @@ const images = importAll(
   require.context("./assets/img/", false, /\.(png|jpe?g|svg)$/)
 );
 
-document.addEventListener('DOMContentLoaded', () => {
-  const projectBtn = document.getElementById('project-btn')
-  projectBtn.addEventListener('click', TodoList.renderProjectInput)
-  const inputDiv = document.getElementById("input-div")
-  inputDiv.addEventListener('click', (e)=> {
-      if(e.target.id == "remove-icon") {
-        TodoList.removeProjectInput()
-      }
-  })
-  inputDiv.addEventListener('click', (e)=> {
-    if(e.target.id == "check-icon") {
-      TodoList.addProject()
-      TodoList.removeProjectInput()
+// import end
+
+// Set project counter in local storage start
+
+if (localStorage.length === 0) {
+  localStorage.setItem("projectCounter", "0");
+}
+
+// Set project counter in local storage end
+
+const bodyBg = () => {
+  document.body.style.background = `linear-gradient(rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.8) 100%), url(${images[0]}) no-repeat`;
+  document.body.style.backgroundSize = "auto, cover";
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  bodyBg();
+  const projectBtn = document.getElementById("project-btn");
+  const projectInput = document.getElementById("project-input");
+  const taskBtn = document.getElementById("task-btn");
+  projectBtn.addEventListener("click", TodoList.renderProjectInput);
+  taskBtn.addEventListener("click", TodoList.renderTaskInput);
+  projectInput.addEventListener("click", (e) => {
+    if (e.target.id == "project-remove") {
+      TodoList.removeProjectInput();
     }
-})
-})
+  });
+  projectInput.addEventListener("click", (e) => {
+    if (e.target.id == "project-check") {
+      TodoList.addProject();
+      TodoList.removeProjectInput();
+    }
+  });
+});
