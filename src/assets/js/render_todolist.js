59e6ecbbc089e43e36574101623e4188b08f3105
NtwaliHeritier <heritierntwali@gmail.com>
2020-12-17 19:10:34 +0200

    fix linter errors

--- src/assets/js/render_todolist.js
+++ src/assets/js/render_todolist.js
@@ -16,36 +16,36 @@ const TodoList = (() => {
 
   // Helpers start
   const addProjectCount = () => {
-    const value = localStorage.getItem("projectCounter");
+    const value = localStorage.getItem('projectCounter');
     const counter = JSON.parse(value) + 1;
     localStorage.projectCounter = JSON.stringify(counter);
     return counter;
   };
 
   const addTaskCount = () => {
-    const value = localStorage.getItem("taskCounter");
+    const value = localStorage.getItem('taskCounter');
     const counter = JSON.parse(value) + 1;
     localStorage.taskCounter = JSON.stringify(counter);
     return counter;
   };
 
   const getProjectIndex = (item) => {
-    const index = item.dataset.index;
+    const { index } = item.dataset;
     return index;
   };
 
   const activeProject = (item) => {
-    const projectLis = document.querySelectorAll(".project-li");
-    const home = document.getElementById("home");
-    home.classList.remove("active");
-    projectLis.forEach((item) => item.classList.remove("active"));
-    item.classList.add("active");
+    const projectLis = document.querySelectorAll('.project-li');
+    const home = document.getElementById('home');
+    home.classList.remove('active');
+    projectLis.forEach((item) => item.classList.remove('active'));
+    item.classList.add('active');
   };
   // Helpers end
 
   // Render methods start
   const renderProjectInput = () => {
-    const inputDiv = document.getElementById("project-input");
+    const inputDiv = document.getElementById('project-input');
     inputDiv.innerHTML = `
           <div data-bs-hover-animate="pulse" style="font-size: 20px;color: rgb(255,255,255);margin-left: -15px;padding-left: 20px;">
           <div><input type="text" id="input" style="width: 120px;border-style: none;border-radius: 15px;margin-right: 10px;font-size: 12px;height: 35px;padding: 0 6px;" placeholder="Enter project name..."><i class="fa fa-check" id="project-check" style="margin-right: 7px;color: rgb(255,193,7);"></i><i class="fa fa-remove" id="project-remove" style="color: rgb(255,193,7);"></i></div>
@@ -54,7 +54,7 @@ const TodoList = (() => {
   };
 
   const renderTaskInput = () => {
-    const taskInput = document.getElementById("new-task-input");
+    const taskInput = document.getElementById('new-task-input');
     taskInput.innerHTML = `
       <div class="row no-gutters d-flex flex-row"
       style="margin: 5px;margin-top: 15px;color: rgb(255,255,255);background: rgba(23,162,184,0.5);border: 0.5px solid rgb(255,193,7) ;border-bottom-style: solid;border-bottom-color: rgb(255,193,7);">
@@ -89,37 +89,35 @@ const TodoList = (() => {
   const renderHeader = (index) => {
     const value = localStorage.getItem(`Project-${index}`);
     const project = JSON.parse(value);
-    const header = document.getElementById("header").querySelector("h3");
+    const header = document.getElementById('header').querySelector('h3');
     header.innerHTML = `MyTodos | ${project.name}`;
   };
 
   const renderTask = (obj) => {
-    const tasks = document.getElementById("tasks");
+    const tasks = document.getElementById('tasks');
     const checkBox = () => {
-      if (obj.status == false) {
-        return "checked";
-      } else {
-        return "";
+      if (obj.status === false) {
+        return 'checked';
       }
+      return '';
     };
     const lineThroughstyle = () => {
-      if (obj.status == false) {
-        return "text-decoration: line-through;";
-      } else {
-        return "";
+      if (obj.status === false) {
+        return 'text-decoration: line-through;';
       }
+      return '';
     };
     tasks.innerHTML += `
       <div data-id="${
-        obj.id
-      }" class="task-target row no-gutters d-flex flex-row" data-bs-hover-animate="pulse"
+  obj.id
+  }" class="task-target row no-gutters d-flex flex-row" data-bs-hover-animate="pulse"
   style="margin: 5px;margin-top: 15px;color: rgb(255,255,255);border-width: 0.5px;border-style: none;border-bottom-style: solid;border-bottom-color: rgb(255,193,7);${lineThroughstyle()}">
   <div class="col-4 col-sm-5 col-md-6 col-lg-5 d-flex justify-content-start align-items-center">
     <div class="form-check"><input class="form-check-input d-lg-flex align-items-lg-center" type="checkbox"
         id="formCheck-6" style="border-radius: 0px;" ${checkBox()} ><label
         class="form-check-label d-lg-flex align-items-lg-center" for="formCheck-1"><strong>${
-          obj.title
-        }</strong></label></div>
+  obj.title
+  }</strong></label></div>
   </div>
   <div
     class="col-2 col-sm-2 col-md-2 col-lg-2 d-flex d-md-flex d-lg-flex justify-content-sm-center justify-content-md-center justify-content-lg-center">
@@ -128,15 +126,15 @@ const TodoList = (() => {
   <div
     class="col-4 col-sm-3 col-md-3 col-lg-4 d-md-flex d-lg-flex justify-content-md-center justify-content-lg-center">
     <span class="d-flex justify-content-center align-items-center">${
-      obj.date
-    }</span>
+  obj.date
+  }</span>
   </div>
   </div>
   `;
   };
 
   const renderProject = (value, index) => {
-    const ul = document.getElementById("project-list");
+    const ul = document.getElementById('project-list');
     ul.innerHTML += `
           <li class="font-weight-bold project-li cursor-pointer" data-bs-hover-animate="pulse" data-index="${index}"
                 style="font-size: 20px;margin-left: -15px;padding-left: 20px;">${value}</li>
@@ -144,7 +142,7 @@ const TodoList = (() => {
   };
 
   const addTaskBtn = () => {
-    const div = document.getElementById("taskDivBtn");
+    const div = document.getElementById('taskDivBtn');
     div.innerHTML = `
     <button id="task-btn" class="btn btn-warning btn-sm float-right" type="button" style="margin-top: 30px;"><i
             class="fa fa-plus"></i><span>Add a new task</span></button>
@@ -155,29 +153,29 @@ const TodoList = (() => {
 
   // Dom and localstorage manipulation start
   const removeProjectInput = () => {
-    const inputDiv = document.getElementById("project-input");
+    const inputDiv = document.getElementById('project-input');
     const div = inputDiv.children[Array.from(inputDiv.children).length - 1];
     inputDiv.removeChild(div);
   };
 
   const removeTaskInput = () => {
-    const taskInput = document.getElementById("new-task-input");
-    taskInput.innerHTML = ``;
+    const taskInput = document.getElementById('new-task-input');
+    taskInput.innerHTML = '';
   };
 
   const addProjectToLocalStorage = () => {
-    const name = document.getElementById("input").value;
+    const name = document.getElementById('input').value;
     const project = new Project(name);
-    let counter = addProjectCount();
+    const counter = addProjectCount();
     localStorage.setItem(`Project-${counter}`, JSON.stringify(project));
     renderProject(name, counter);
   };
 
   const addTask = (index) => {
-    const title = document.getElementById("task_title").value;
-    const priority = document.getElementById("task_priority").value;
-    const date = document.getElementById("task_date").value;
-    let taskCounter = addTaskCount();
+    const title = document.getElementById('task_title').value;
+    const priority = document.getElementById('task_priority').value;
+    const date = document.getElementById('task_date').value;
+    const taskCounter = addTaskCount();
     const id = [index, taskCounter.toString()];
     const taskObj = new Task(title, priority, date, id);
     const lsProject = localStorage.getItem(`Project-${index}`);
@@ -190,17 +188,17 @@ const TodoList = (() => {
   const showProjects = () => {
     for (let i = 0; i < localStorage.length; i += 1) {
       const key = localStorage.key(i);
-      if (key.startsWith("Project-")) {
+      if (key.startsWith('Project-')) {
         const value = JSON.parse(localStorage[key]);
-        const index = key.valueOf().replace(/\D/g, "");
+        const index = key.valueOf().replace(/\D/g, '');
         renderProject(value.name, index);
       }
     }
   };
 
   const showTasks = (index) => {
-    const tasks = document.getElementById("tasks");
-    tasks.innerHTML = ``;
+    const tasks = document.getElementById('tasks');
+    tasks.innerHTML = '';
     const value = localStorage.getItem(`Project-${index}`);
     const project = JSON.parse(value);
     for (let i = 0; i < project.tasks.length; i += 1) {
@@ -208,23 +206,23 @@ const TodoList = (() => {
     }
   };
 
-  const clickCheckBox = () => {
-    const check = document.querySelector("#tasks");
-    check.addEventListener("click", (e) => {
-      if (e.target.className.includes("form-check-input")) {
-        const idArr = e.target.closest(".task-target").dataset.id;
-        renderLineThrough(idArr, e.target.closest(".task-target"));
-      }
-    });
+  const strikeThrough = (task, obj) => {
+    if (task.status === false) {
+      obj.style.textDecoration = 'line-through';
+      obj.querySelector('.form-check-input').checked = true;
+    } else {
+      obj.style.textDecoration = 'none';
+      obj.querySelector('.form-check-input').checked = false;
+    }
   };
 
   const renderLineThrough = (id, obj) => {
-    const pid = id.split(",")[0];
+    const pid = id.split(',')[0];
     const project = localStorage.getItem(`Project-${pid}`);
     const parsedProject = JSON.parse(project);
     parsedProject.tasks.map((task) => {
-      if (JSON.stringify(task.id) == JSON.stringify(id.split(","))) {
-        let index = parsedProject.tasks.indexOf(task);
+      if (JSON.stringify(task.id) === JSON.stringify(id.split(','))) {
+        const index = parsedProject.tasks.indexOf(task);
         task.status = !task.status;
         parsedProject.tasks[index] = task;
         strikeThrough(task, obj);
@@ -233,29 +231,29 @@ const TodoList = (() => {
     localStorage[`Project-${pid}`] = JSON.stringify(parsedProject);
   };
 
-  const strikeThrough = (task, obj) => {
-    if (task.status == false) {
-      obj.style.textDecoration = "line-through";
-      obj.querySelector(".form-check-input").checked = true;
-    } else {
-      obj.style.textDecoration = "none";
-      obj.querySelector(".form-check-input").checked = false;
-    }
+  const clickCheckBox = () => {
+    const check = document.querySelector('#tasks');
+    check.addEventListener('click', (e) => {
+      if (e.target.className.includes('form-check-input')) {
+        const idArr = e.target.closest('.task-target').dataset.id;
+        renderLineThrough(idArr, e.target.closest('.task-target'));
+      }
+    });
   };
 
   const showAllTasks = () => {
-    const header = document.getElementById("header").querySelector("h3");
-    header.innerHTML = `MyTodos | Home`;
+    const header = document.getElementById('header').querySelector('h3');
+    header.innerHTML = 'MyTodos | Home';
     const taskArr = [];
     for (let i = 0; i < localStorage.length; i += 1) {
       const key = localStorage.key(i);
-      if (key.startsWith("Project-")) {
+      if (key.startsWith('Project-')) {
         const value = JSON.parse(localStorage[key]);
         value.tasks.forEach((task) => taskArr.push(task));
       }
     }
-    const tasks = document.getElementById("tasks");
-    tasks.innerHTML = ``;
+    const tasks = document.getElementById('tasks');
+    tasks.innerHTML = '';
 
     for (let i = 0; i < taskArr.length; i += 1) {
       renderTask(taskArr[i]);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // import start
 
-import "./assets/css/styles.min.css";
-import TodoList from "./assets/js/render_todolist";
+import './assets/css/styles.min.css';
+import TodoList from './assets/js/render_todolist';
 
 const importAll = (r) => r.keys().map(r);
-importAll(require.context("./assets/js/", true, /\.js$/));
+importAll(require.context('./assets/js/', true, /\.js$/));
 
 const images = importAll(
-  require.context("./assets/img/", false, /\.(png|jpe?g|svg)$/)
+  require.context('./assets/img/', false, /\.(png|jpe?g|svg)$/),
 );
 
 // import end
@@ -15,67 +15,67 @@ const images = importAll(
 // Set project counter in local storage start
 
 if (localStorage.length === 0) {
-  localStorage.setItem("projectCounter", "0");
-  localStorage.setItem("taskCounter", "0");
+  localStorage.setItem('projectCounter', '0');
+  localStorage.setItem('taskCounter', '0');
 }
 
 // Set project counter in local storage end
 
 const bodyBg = () => {
   document.body.style.background = `linear-gradient(rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.8) 100%), url(${images[0]}) no-repeat`;
-  document.body.style.backgroundSize = "auto, cover";
+  document.body.style.backgroundSize = 'auto, cover';
 };
 
 TodoList.clickCheckBox();
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   bodyBg();
   TodoList.showAllTasks();
   TodoList.showProjects();
-  const projectBtn = document.getElementById("project-btn");
-  const projectInput = document.getElementById("project-input");
-  const taskInput = document.getElementById("new-task-input");
-  const home = document.getElementById("home");
-  home.addEventListener("click", () => {
-    const btn = document.getElementById("taskDivBtn");
-    btn.innerHTML = ``;
-    home.classList.add("active");
-    const projectLis = document.querySelectorAll(".project-li");
-    projectLis.forEach((item) => item.classList.remove("active"));
+  const projectBtn = document.getElementById('project-btn');
+  const projectInput = document.getElementById('project-input');
+  const taskInput = document.getElementById('new-task-input');
+  const home = document.getElementById('home');
+  home.addEventListener('click', () => {
+    const btn = document.getElementById('taskDivBtn');
+    btn.innerHTML = '';
+    home.classList.add('active');
+    const projectLis = document.querySelectorAll('.project-li');
+    projectLis.forEach((item) => item.classList.remove('active'));
     TodoList.showAllTasks();
   });
 
-  projectBtn.addEventListener("click", TodoList.renderProjectInput);
+  projectBtn.addEventListener('click', TodoList.renderProjectInput);
 
-  projectInput.addEventListener("click", (e) => {
-    if (e.target.id == "project-check") {
+  projectInput.addEventListener('click', (e) => {
+    if (e.target.id === 'project-check') {
       TodoList.addProjectToLocalStorage();
       TodoList.removeProjectInput();
-    } else if (e.target.id == "project-remove") {
+    } else if (e.target.id === 'project-remove') {
       TodoList.removeProjectInput();
     }
   });
 
-  taskInput.addEventListener("click", (e) => {
-    if (e.target.id == "task-check") {
-      const index = document.querySelector(".active").dataset.index;
+  taskInput.addEventListener('click', (e) => {
+    if (e.target.id === 'task-check') {
+      const { index } = document.querySelector('.active').dataset;
       TodoList.addTask(index);
       TodoList.removeTaskInput();
-    } else if (e.target.id == "task-remove") {
+    } else if (e.target.id === 'task-remove') {
       TodoList.removeTaskInput();
     }
   });
 
-  const ul = document.getElementById("project-list");
-  ul.addEventListener("click", (e) => {
-    if (e.target.tagName === "LI") {
+  const ul = document.getElementById('project-list');
+  ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'LI') {
       const index = TodoList.getProjectIndex(e.target);
       TodoList.activeProject(e.target);
       TodoList.renderHeader(index);
       TodoList.showTasks(index);
       TodoList.addTaskBtn();
-      const taskBtn = document.getElementById("task-btn");
-      taskBtn.addEventListener("click", TodoList.renderTaskInput);
+      const taskBtn = document.getElementById('task-btn');
+      taskBtn.addEventListener('click', TodoList.renderTaskInput);
     }
   });
 });
