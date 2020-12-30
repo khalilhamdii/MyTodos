7eff8a2a29185d39f9ab49bb27cc94d1f75058b4
khalilhamdii <khaalil.hamdi@gmail.com>
2020-12-30 15:38:14 +0100

    Add test cases for constructor functions

--- src/_test_/test_fles/constructors.test.js
+++ src/_test_/test_fles/constructors.test.js
@@ -1,6 +1,17 @@
 import { Project, Task } from "../functions/constructors";
 
-test("just a test", () => {
-  const a = 1 + 1;
-  expect(a).toBe(2);
+test("Expect Project constructor to create a new project object", () => {
+  const newProject = new Project("Some Project");
+  expect(newProject).toEqual({ name: "Some Project", tasks: [] });
+});
+
+test("Expect Task constructor to create a new task object", () => {
+  const newTask = new Task("Task 1", "High", "30-12-2020", [1, 1]);
+  expect(newTask).toEqual({
+    title: "Task 1",
+    priority: "High",
+    date: "30-12-2020",
+    id: [1, 1],
+    status: true,
+  });
 });
